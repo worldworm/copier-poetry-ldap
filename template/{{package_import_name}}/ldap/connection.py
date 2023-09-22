d470359e0259e24f6254907c93eee64c4a3bd26e
worldworm <13227454+worldworm@users.noreply.github.com>
2023-09-22 22:19:30 +0200

    adds ldap connection server pool

--- template/{{package_import_name}}/ldap/connection.py
+++ template/{{package_import_name}}/ldap/connection.py
@@ -1,7 +1,7 @@
 """LDAP connection module."""
 import os
 
-from ldap3 import NTLM, Connection
+from ldap3 import Connection, Server, ServerPool
 from loguru import logger
 
 from ..config import config
@@ -14,7 +14,14 @@ def cleanup():
 
 
 if not os.environ.get("PYTEST"):  # pragma: no cover
-    connection = Connection(config.LDAP_URL, auto_bind=True, user=config.LDAP_USER, password=config.LDAP_PASSWORD, authentication=NTLM)
+    if isinstance(config.LDAP_URL, list):
+        server = ServerPool(None, "ROUND_ROBIN")
+        for server_url in config.LDAP_URL:
+            server.add(Server(server_url))
+    else:
+        server = Server(config.LDAP_URL)
+
+    connection = Connection(server, auto_bind=True, user=config.LDAP_USER, password=config.LDAP_PASSWORD)
     logger.debug("ldap connection established with user {}", connection.extend.standard.who_am_i())
 else:
     connection = None  # pylint: disable=invalid-name
